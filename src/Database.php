9da8b5f8417627728c63370d23d237e1c070350f
Fabien Di Tore <fabien@ditore.ch>
2017-01-22 11:46:40 +0100

    ok

--- src/Database.php
+++ src/Database.php
@@ -1,8 +1,8 @@
 <?php
 namespace FDT2k\Libs;
 
-use \ICE\lib\helpers as helpers;
-use \ICE\Env as Env;
+use \FDT2k\Helpers as helpers;
+use \FDT2k\ICE\CORE\Env as Env;
 
 define('DB_TYPE_MYSQL',1);
 define('DB_TYPE_MSSQL',2);
