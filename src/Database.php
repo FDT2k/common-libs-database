a7aa06ce7a29b8ceaad23653e068fe6e623cac4a
Fabien Di Tore <fabien@ditore.ch>
2017-01-28 15:20:03 +0100

    ok

--- src/Database.php
+++ src/Database.php
@@ -2,7 +2,7 @@
 namespace FDT2k\Libs;
 
 use \FDT2k\Helpers as helpers;
-use \FDT2k\ICE\CORE\Env as Env;
+use \FDT2k\Noctis\Core\Env as Env;
 
 define('DB_TYPE_MYSQL',1);
 define('DB_TYPE_MSSQL',2);
--- src/DatabaseMYSQLI.php
+++ src/DatabaseMYSQLI.php
@@ -1,7 +1,7 @@
 <?php
 namespace FDT2k\Libs;
 use \FDT2k\Helpers as helpers;
-use \FDT2k\ICE\CORE\Env as Env;
+use \FDT2k\Noctis\Core\Env as Env;
 use \ICE\lib\scaffolding as sf;
 /**********************************
 Mysql Database class
